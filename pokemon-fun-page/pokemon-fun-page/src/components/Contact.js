3619c7f28e07506c34c6fa5badce5f6744ec0d7c
madhumudunuri <mmudunuri@serenova.com>
2020-03-31 07:48:23 +0530

    programmatic redirects and higherOrderComponents

--- pokemon-fun-page/pokemon-fun-page/src/components/Contact.js
+++ pokemon-fun-page/pokemon-fun-page/src/components/Contact.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
+    // console.log(props)
+    setTimeout(() => {
+        props.history.push('/about')
+    }, 2000)
     return (
         <div className="container">
             <p className="center">Welcome to Contact Page!!!</p>
