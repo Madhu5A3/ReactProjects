b2832596d84ce7d289a7a28c30913a03ccd6cfac
madhumudunuri <mmudunuri@serenova.com>
2020-03-31 10:45:14 +0530

    Route Parameters

--- pokemon-fun-page/pokemon-fun-page/src/App.js
+++ pokemon-fun-page/pokemon-fun-page/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Post from './components/Post';
 
 class App extends Component {
   render(){
@@ -14,6 +15,7 @@ class App extends Component {
           <Route exact path="/" component={Home}></Route>
           <Route path="/about" component={About}></Route>
           <Route path="/contact" component={Contact}></Route>
+          <Route path="/posts/:post_id" component={Post}></Route>
         </div>
       </BrowserRouter>
     );
