c194480609803ffae1551fdd5724a0d082ba1364
madhumudunuri <mmudunuri@serenova.com>
2020-03-31 10:48:20 +0530

    Switch Tag

--- pokemon-fun-page/pokemon-fun-page/src/App.js
+++ pokemon-fun-page/pokemon-fun-page/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -12,10 +12,12 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path="/" component={Home}></Route>
-          <Route path="/about" component={About}></Route>
-          <Route path="/contact" component={Contact}></Route>
-          <Route path="/posts/:post_id" component={Post}></Route>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route path="/contact" component={Contact}></Route>
+            <Route path="/:post_id" component={Post}></Route>
+          </Switch>
         </div>
       </BrowserRouter>
     );
